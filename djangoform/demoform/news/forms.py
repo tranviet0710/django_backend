f4880701fa3a878762fa94479c822d51e5bc0025
VietDev <102190298@sv1.dut.udn.vn>
2022-04-26 12:22:27 +0700

    django from widgets 26.04.2022

--- djangoform/demoform/news/forms.py
+++ djangoform/demoform/news/forms.py
@@ -1,15 +1,19 @@
 from django import forms
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title','content', 'time_create',)
+        fields = ('title', 'content', 'time_create',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'tieude123'}),
+            'content': forms.Textarea(attrs={'class': 'noidung123'})
+        }
 
 
 class SendEmail(forms.Form):
-    title = forms.CharField(max_length=100)
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'tieude'}))
     email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'vietdev', 'id': 'noidung'}))
     cc = forms.BooleanField(required=False)
-
