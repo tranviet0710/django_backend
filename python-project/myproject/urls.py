8652da3bfac9a40d7efc962a6de2f5401223eec2
VietDev <102190298@sv1.dut.udn.vn>
2022-04-27 19:48:18 +0700

    python-project 27.04.2022

---
+++ python-project/myproject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('polls/', include("polls.urls")),
+    path('polls2/', include("polls2.urls")),
+    path('admin/', admin.site.urls),
+]
