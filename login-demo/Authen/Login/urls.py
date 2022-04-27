6a7f790652a2d75db664e588ab71b95f3d107202
VietDev <102190298@sv1.dut.udn.vn>
2022-04-28 01:06:14 +0700

    django login user 28.04.2022

--- login-demo/Authen/Login/urls.py
+++ login-demo/Authen/Login/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+
 from . import views
+
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('login/', views.LoginClass.as_view(), name='login')
+    path('login/', views.LoginClass.as_view(), name='login'),
+    path('user-view/', views.ViewUser.as_view(), name='user_view')
 ]
--- login-demo/Authen/Login/views.py
+++ login-demo/Authen/Login/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
-from django.contrib.auth import authenticate
+
+
 # Create your views here.
 class Index(View):
     def get(self, request):
         return HttpResponse("<h1>Xin Chao</h1>")
 
+
 class LoginClass(View):
     def get(self, request):
         return render(request, 'Login/login.html')
+
     def post(self, request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         account = authenticate(username=username, password=password)
         if account is None:
-            return HttpResponse("Account is not existed")
-        return HttpResponse("Login with %s %s" %(username,password))
+            return HttpResponse("Login Unsuccessfully! Account is not existed")
+
+        login(request, account)
+
+        return render(request, 'Login/successful.html')
+
+
+class ViewUser(View):
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return HttpResponse("Please login!")
+        else:
+            return HttpResponse("<h1>This is view user</h1>")
