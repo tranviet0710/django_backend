3f0a1d263821db5a136cce8be0b063b2f3221d75
VietDev <trancongviet0710@gmail.com>
2022-05-12 11:31:24 +0700

    custom admin site django 12.05.2022

--- login-demo/Authen/Authen/settings.py
+++ login-demo/Authen/Authen/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'Authen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,8 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 
+]
 AUTH_USER_MODEL = 'Login.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
